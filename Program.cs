e6a39d84fc65f20d8277237cc1303a35c81d9ab3
Jonathan Oliveira Home <jonathan.oliveira@aluno.ifsp.edu.br>
2020-03-31 03:21:53 -0300

    init project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace introducao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  numero = 5;
+            
+            for(int i=0; i<numero;i++)
+            {
+                Console.WriteLine("teste");
+
+            }
+
+            
+        }
+    }
+}
